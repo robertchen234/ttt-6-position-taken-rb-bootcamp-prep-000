c391dccde74cee139fb98bd280bc2d967366426d
Robert Chen <robertchen234@gmail.com>
2018-10-01 19:48:48 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 
 position_taken?(board, index)
-  if
-    elsif
-    elsif
-    else
+  if board[index] = " " || board[index] != "" || board[index] != nil
+    return false
+  elsif board[inde]
+    return true
   end
 end
